139f8d60e2308ae992ee0828686369d631b3bba0
Jasonmmill <jasonmatthewmill@gmail.com>
2025-02-09 20:09:29 -0500

    Task 3 - Shopping Cart

--- cc_5.js
+++ cc_5.js
@@ -1,20 +1,29 @@
 // Task 1: Object Properties
-let customer = {
+let customer = { // create object
     name: "John Doe",
     age: 35,
     email: "john.doe@email.com"
-}
+} // log objects
 console.log(`Customer Name: ${customer.name}`)
 console.log(`Customer Age: ${customer.age}`)
 console.log(`Customer Email: ${customer.email}`)
 
 // Task 2: Order Details
-let order = {
+let order = { // create object
     orderId: 12345,
     totalAmount: 150,
     status: "Processing",
-    displayOrder() {
+    displayOrder() { // create method to return details
         return `Order ID: ${this.orderId}, Total Amount: ${this.totalAmount}, Status: ${this.status}`
     }
-}
+} // return details
 console.log(order.displayOrder())
+
+// Task 3: Array Manipulation
+let cartItems = [ "apple", "banana", "orange" ] // create array
+cartItems.push("mango") // add item to end
+cartItems.pop() // remove end item
+cartItems.unshift("mango") // add item to beginning
+cartItems.shift() // remove item from beginning
+console.log(cartItems)
+
