0c70249abf182d873be78009e0ad25a626503c9c
Jasonmmill <jasonmatthewmill@gmail.com>
2025-02-09 20:03:04 -0500

    Task 2 - Order Details

--- cc_5.js
+++ cc_5.js
@@ -7,3 +7,14 @@ let customer = {
 console.log(`Customer Name: ${customer.name}`)
 console.log(`Customer Age: ${customer.age}`)
 console.log(`Customer Email: ${customer.email}`)
+
+// Task 2: Order Details
+let order = {
+    orderId: 12345,
+    totalAmount: 150,
+    status: "Processing",
+    displayOrder() {
+        return `Order ID: ${this.orderId}, Total Amount: ${this.totalAmount}, Status: ${this.status}`
+    }
+}
+console.log(order.displayOrder())
